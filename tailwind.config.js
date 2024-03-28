4f1cbdc82a02605e75ec5cd005c4f0a2bbb2f26c
michael <maroc.michael@hotmail.com>
2024-03-29 00:02:10 +0100

    add tailwind color styles

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "#EAEAEA",
+        "light-gray": "#F6F6F6",
+        "color-primary": "9356DC",
+        "color-secondary": "#FF79DA",
+      },
+    },
+  },
+  plugins: [],
+};

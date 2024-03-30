1dacff25e3782d2e9d890b2c90dcc65cb2e0f414
michael <maroc.michael@hotmail.com>
2024-03-30 22:33:36 +0100

    update home display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { FunctionningComponent } from '../../components/functionning/functionning.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RestaurantsComponent } from '../../components/restaurants/restaurants.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FunctionningComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FunctionningComponent,
+    RestaurantsComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })

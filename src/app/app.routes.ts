bc6f664a996e918d588b57ed343c5804e3bf3b7a
michael <maroc.michael@hotmail.com>
2024-04-03 02:48:42 +0200

    add menu route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu/:id', component: MenuComponent },
+];

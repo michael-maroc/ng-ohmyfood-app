448f21396e448cd8d40021734cb9c695df64260a
michael <maroc.michael@hotmail.com>
2024-04-03 17:18:20 +0200

    add navigation arrow

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Restaurant } from '../../shared/model/Restaurant';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { CommonModule } from '@angular/common';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, FooterComponent, FontAwesomeModule],
+  imports: [CommonModule, FooterComponent, FontAwesomeModule, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
@@ -17,6 +18,7 @@ export class MenuComponent implements OnInit {
   resturantList = Restaurant;
   restaurant: any;
   faHeart = faHeart;
+  faArrowLeft = faArrowLeft;
 
   constructor(private route: ActivatedRoute) {}
 
